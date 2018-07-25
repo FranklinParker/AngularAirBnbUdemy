a9a75de799fa327bb7a9f80c8cec5dc0e4bb2409
Franklin Parker <fparker@deliveryman.com>
2018-07-25 18:02:03 -0400

    merge

--- src/app/rental/components/rental-detail/rental-booking/rental-booking.component.ts
+++ src/app/rental/components/rental-detail/rental-booking/rental-booking.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit, Input,ViewChild } from '@angular/core';
+import {Component, OnInit, Input, ViewChild} from '@angular/core';
 import {Rental} from '../../../models/rental';
-import { Daterangepicker,DaterangePickerComponent} from 'ng2-daterangepicker';
+import {Daterangepicker, DaterangePickerComponent} from 'ng2-daterangepicker';
 
 @Component({
   selector: 'bwm-rental-booking',
@@ -9,7 +9,13 @@ import { Daterangepicker,DaterangePickerComponent} from 'ng2-daterangepicker';
 })
 
 export class RentalBookingComponent implements OnInit {
-  @Input() rental: Rental;
+  @Input() price: any;
+  public daterange: any = {};
+  public options: any = {
+    locale: {format: 'YYYY-MM-DD'},
+    alwaysShowCalendars: false,
+    opens: 'left'
+  };
   @ViewChild(DaterangePickerComponent)
   private picker: DaterangePickerComponent;
 
@@ -19,25 +25,11 @@ export class RentalBookingComponent implements OnInit {
   ngOnInit() {
   }
 
-  public daterange: any = {};
-
-  // see original project for full list of options
-  // can also be setup using the config service to apply to multiple pickers
-  public options: any = {
-    locale: {format: 'YYYY-MM-DD'},
-    alwaysShowCalendars: false,
-    opens: 'left'
-  };
-
   public selectedDate(value: any, datepicker?: any) {
-    // this is the date the iser selected
-    console.log(value);
 
-    // any object can be passed to the selected event and it will be passed back here
     datepicker.start = value.start;
     datepicker.end = value.end;
 
-    // or manupulat your own internal property
     this.daterange.start = value.start;
     this.daterange.end = value.end;
     this.daterange.label = value.label;
