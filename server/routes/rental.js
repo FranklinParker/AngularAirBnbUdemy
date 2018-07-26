2d96f3aaffc72e51cb45422a1e8b9027c6a99cbd
Franklin Parker <fparker@deliveryman.com>
2018-07-25 21:38:12 -0400

    booking

--- server/routes/rental.js
+++ server/routes/rental.js
@@ -4,27 +4,33 @@ const userCtl = require('../controllers/user');
 
 const router = express.Router();
 
-router.get('/secret', userCtl.authMiddleware, (req,res)=>{
- res.json({'secret': true});
+router.get('/secret', userCtl.authMiddleware, (req, res) => {
+  res.json({'secret': true});
 });
 
-router.get('', (req,res)=>{
-  Rental.find({}, function (err, rentals) {
-    res.json(rentals);
-  });
+router.get('', (req, res) => {
+  Rental.find({})
+    .select('-bookings')
+    .exec(function (err, foundRentals) {
+      res.json(foundRentals);
+    });
 
 });
 
-router.get('/:id', (req,res)=>{
-  const id = req.params.id;
-  Rental.findById(id, function (err, rental) {
-    if(err){
-      res.status(422).send({ errors: [{ title: 'Rental Errors!', detail: ' Could not Find Rental'}]})
+router.get('/:id', (req, res) => {
+  const rentalId = req.params.id;
 
-    }else {
-      res.json(rental);
-    }
-  });
+  Rental.findById(rentalId)
+    .populate('user', 'username -_id')
+    .populate('bookings', 'startAt endAt -_id')
+    .exec(function(err, foundRental) {
+
+      if (err) {
+        return res.status(422).send({errors: [{title: 'Rental Error!', detail: 'Could not find Rental!'}]});
+      }
+
+      return res.json(foundRental);
+    });
 
 });
 
