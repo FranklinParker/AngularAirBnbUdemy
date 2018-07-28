b6d54af4ab182a105098c7becb9ddac6257a0a03
Franklin Parker <fparker@deliveryman.com>
2018-07-28 15:05:49 -0400

    layout

--- src/app/common/header/header.component.ts
+++ src/app/common/header/header.component.ts
@@ -8,7 +8,7 @@ import {Router} from '@angular/router';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
-
+  searchCity: string ='boston';
   constructor(public auth: AuthService,
               private router: Router) { }
 
