432c82144758d94d8e03b5dc65baff080739431f
Franklin Parker <fparker@deliveryman.com>
2018-07-27 22:18:06 -0400

    fixed

--- server/routes/rental.js
+++ server/routes/rental.js
@@ -1,4 +1,5 @@
 const Rental = require('../models/rental');
+const User = require('../models/user');
 const express = require('express');
 const userCtl = require('../controllers/user');
 
@@ -15,7 +16,7 @@ router.get('/:id', (req, res) => {
   Rental.findById(rentalId)
     .populate('user', 'username -_id')
     .populate('bookings', 'startAt endAt -_id')
-    .exec(function(err, foundRental) {
+    .exec(function (err, foundRental) {
 
       if (err) {
         return res.status(422).send({errors: [{title: 'Rental Error!', detail: 'Could not find Rental!'}]});
@@ -29,17 +30,22 @@ router.get('/:id', (req, res) => {
 
 router.get('', (req, res) => {
   const city = req.query.city;
-  const query = city ? {city : new RegExp(city, 'i')} : {};
+  const query = city ? {city: new RegExp(city, 'i')} : {};
   Rental.find(query)
     .select('-bookings')
-    .exec(function(err, foundRentals) {
+    .exec(function (err, foundRentals) {
 
       if (err) {
         return res.status(422).send({errors: normalizeErrors(err.errors)});
       }
 
       if (city && foundRentals.length === 0) {
-        return res.status(422).send({errors: [{title: 'No Rentals Found!', detail: `There are no rentals for city ${city}`}]});
+        return res.status(422).send({
+          errors: [{
+            title: 'No Rentals Found!',
+            detail: `There are no rentals for city ${city}`
+          }]
+        });
       }
 
       return res.json(foundRentals);
@@ -48,22 +54,23 @@ router.get('', (req, res) => {
 });
 
 
+router.post('', userCtl.authMiddleware, function (req, res) {
+  const {title, city, street, category, image, shared, bedrooms, description, dailyRate} = req.body;
+  const user = res.locals.user;
 
-router.post('',  userCtl.authMiddleware, (req, res) => {
-  const rentalData = req.body;
-  rentalData.user =  res.locals.user;
-  rentalData.bookings  = [];
-  console.log('rentalData', rentalData);
-  const rental = new Rental(
-    rentalData
-  );
-  rental.save(function(err, rental) {
+  const rental = new Rental({title, city, street, category, image, shared, bedrooms, description, dailyRate});
+  rental.user = user;
+
+  Rental.create(rental, function (err, newRental) {
     if (err) {
       return res.status(422).send({errors: normalizeErrors(err.errors)});
     }
-    return res.json(rental);
-  });
 
+    User.update({_id: user.id}, {$push: {rentals: newRental}}, function () {
+    });
+
+    return res.json(newRental);
+  });
 });
 
 
