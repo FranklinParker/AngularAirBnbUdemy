8403329ce4d4d53bf5e844ae2d58479e6119a9e1
Franklin Parker <fparker@deliveryman.com>
2018-07-30 16:23:05 -0400

    err handling

--- src/app/manage/components/manage-rental/manage-rental.component.ts
+++ src/app/manage/components/manage-rental/manage-rental.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {RentalService} from '../../../rental/services/rental.service';
 import {Rental} from '../../../rental/models/rental';
+import {ToastrService} from 'ngx-toastr';
+import {HttpErrorResponse} from '@angular/common/http';
 
 @Component({
   selector: 'bwm-manage-rental',
@@ -10,29 +12,34 @@ import {Rental} from '../../../rental/models/rental';
 export class ManageRentalComponent implements OnInit {
   rentals: Rental[];
   rentalDeleteIndex: number;
-  constructor(private rentalService: RentalService) { }
+
+  constructor(private rentalService: RentalService,
+              private toastrServ: ToastrService,) {
+  }
 
   ngOnInit() {
     this.rentalService.getUserRentals()
-      .subscribe((rentals:Rental[])=>{
+      .subscribe((rentals: Rental[]) => {
         this.rentals = rentals;
 
-      })
+      });
   }
 
-  deleteRental(rental:Rental){
-    console.log('delete', rental);
+  deleteRental(rental: Rental) {
     this.rentalDeleteIndex = undefined;
     this.rentalService.deleteRental(rental)
-      .subscribe((result)=>{
-        this.rentals.splice(this.rentalDeleteIndex,1);
-        this.rentalDeleteIndex = undefined;
-      })
+      .subscribe((result) => {
+          this.rentals.splice(this.rentalDeleteIndex, 1);
+          this.rentalDeleteIndex = undefined;
+        },
+        (error: HttpErrorResponse) => {
+          this.toastrServ.error(error.error.errors[0].detail, 'Cannot Delete!');
+        });
   }
 
 
-  activeBookings(rental:Rental): boolean{
-    return rental.bookings.length>0;
+  activeBookings(rental: Rental): boolean {
+    return false; //rental.bookings.length>0;
   }
 
 }
