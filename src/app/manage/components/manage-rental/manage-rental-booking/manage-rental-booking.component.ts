2c92e9a95459c27b8291d20f447f5cb3b28baeed
Franklin Parker <fparker@deliveryman.com>
2018-07-30 14:36:56 -0400

    show bookings

--- src/app/manage/components/manage-rental/manage-rental-booking/manage-rental-booking.component.ts
+++ src/app/manage/components/manage-rental/manage-rental-booking/manage-rental-booking.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {NgbModal} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'bwm-manage-rental-booking',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ManageRentalBookingComponent implements OnInit {
 
-  constructor() { }
+  constructor(public modalService: NgbModal) { }
 
   ngOnInit() {
   }
