3744c37995f33c969e8b356592ee4e1966db2663
Franklin Parker <fparker@deliveryman.com>
2018-08-28 17:53:44 -0400

    image

--- src/app/common/components/image-upload/image-upload.component.ts
+++ src/app/common/components/image-upload/image-upload.component.ts
@@ -1,5 +1,10 @@
 import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 
+class FileSnippet {
+  constructor(public src:string, public file:File){
+
+  }
+}
 @Component({
   selector: 'bwm-image-upload',
   templateUrl: './image-upload.component.html',
@@ -8,12 +13,22 @@ import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 export class ImageUploadComponent implements OnInit {
   @Output() imageUploaded = new EventEmitter<any>();
   @Output() imageError = new EventEmitter();
+  seletedFile: FileSnippet;
   constructor() { }
 
   ngOnInit() {
   }
-  processFile(imageInput: File){
-    console.log(imageInput);
+  processFile(file: File){
+
+    const reader = new FileReader();
+
+    reader.addEventListener('load', (event:any)=>{
+      this.seletedFile = new FileSnippet(event.target.result, file);
+      console.log(this.seletedFile);
+
+    });
+
+    reader.readAsDataURL(file);
   }
 
 }
