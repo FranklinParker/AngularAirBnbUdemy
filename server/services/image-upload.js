e084b17a000a8eb1d4545e206168c67d3acaf88b
Franklin Parker <fparker@deliveryman.com>
2018-08-28 15:37:18 -0400

    s3

--- server/services/image-upload.js
+++ server/services/image-upload.js
@@ -6,7 +6,7 @@ const config = require('../config');
 aws.config.update({
   secretAccessKey: config.AWS_SECRET_ACCESS_KEY,
   accessKeyId: config.AWS_ACCESS_KEY_ID,
-  region: 'us-east-1'
+  region: 'us-east-2'
 });
 
 const s3 = new aws.S3();
