96de3de6b3ec15dde7afce2ebec0d90cf40731e1
Franklin Parker <fparker@deliveryman.com>
2018-09-04 17:40:00 -0400

    test

--- src/app/common/components/editable/editable.module.ts
+++ src/app/common/components/editable/editable.module.ts
@@ -4,21 +4,26 @@ import {EditableInputComponent} from './editable-input/editable-input.component'
 import {FormsModule} from '@angular/forms';
 import { EditableTextareaComponent } from './editable-textarea/editable-textarea.component';
 import { EditableSelectComponent } from './editable-select/editable-select.component';
+import { EditableImageComponent } from './editable-image/editable-image.component';
+import {SharedModule} from '../../../shared/shared.module';
 
 @NgModule({
   imports: [
     CommonModule,
-    FormsModule
+    FormsModule,
+    SharedModule
   ],
   declarations: [
     EditableInputComponent,
     EditableTextareaComponent,
-    EditableSelectComponent
+    EditableSelectComponent,
+    EditableImageComponent
   ],
   exports:[
     EditableInputComponent,
     EditableTextareaComponent,
-    EditableSelectComponent
+    EditableSelectComponent,
+    EditableImageComponent
   ]
 })
 export class EditableModule { }
